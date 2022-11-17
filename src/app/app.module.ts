fa8271f2e280ad45ed4a6a6ca16d99ae63820ece
Hugo Oyola <hugo.ham@outlook.com>
2022-11-16 23:56:08 -0500

    Rutas de la App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 // Modulo personalizado
-import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    SharedModule,
-    PrimeNgModule
+    AppRouterModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
